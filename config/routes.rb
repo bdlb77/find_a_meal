ef1a26b16592b2feb6ce146acf10306942319e15
Bryan Leighton <bryan.leigh77@gmail.com>
2018-06-04 15:09:34 +0300

    nested bookings under user

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :events, only: [:index, :show, :new, :create, :edit, :delete] do
+  resources :events, only: [:index, :show, :new, :create, :edit, :delete] 
+  resources :users do
   	resources :bookings, only: [:new, :create, :show, :index, :edit, :delete]
   end
 end
