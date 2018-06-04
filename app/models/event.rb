14db17475f2653d4713bf1ff0b86c251ba77d534
adi machnes glinert <amachnes@gmail.com>
2018-06-04 12:22:36 +0300

    added a name to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,14 @@
 class Event < ApplicationRecord
+  has_many :bookings, dependent: :destroy;
+  validates :name, presence: true, uniqueness: true
+
+   t.string "location"
+    t.date "date"
+    t.time "time"
+    t.integer "min_p"
+    t.integer "max_p"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+
 end
