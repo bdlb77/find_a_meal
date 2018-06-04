db82f029365d5e3100e4630db15105bfd20f3578
Bryan Leighton <bryan.leigh77@gmail.com>
2018-06-04 12:53:07 +0300

    setup of bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,46 @@
 class BookingsController < ApplicationController
+	def index
+		@bookings = Booking.all
+	end
+
+	def new
+		@booking = Booking.new
+	end
+
+	def create
+		Booking.create(booking_params)
+		redirect_to booking(booking)
+	end
+
+	def edit
+  end
+
+	def update
+	  @booking
+    if @booking.save
+      @booking.update(booking_params)
+      redirect_to bookings_path
+    else
+      render :edit
+    end
+  end
+
+	def show
+  end
+
+	def delete
+	  @booking.destroy
+    redirect_to bookings_path
+  end
+
+	private
+
+	def booking_params
+		@booking = params.require(:booking).permit(:event, :user)
+	end
+	
+	def set_booking
+   @booking = Bookking.find(params[:id])
+ end
+
 end
