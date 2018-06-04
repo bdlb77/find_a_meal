c7f803c62a3879076874f88f75303064eae78f5f
Bryan Leighton <bryan.leigh77@gmail.com>
2018-06-04 14:30:56 +0300

    added faker gem and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+# TODO: Write a seed to insert 100 posts in the database
+100.times do
+  post = Post.new(
+    location: Faker::Address.community,
+    date: Faker::Internet,
+    time: Faker::Time.forward(23, :morning),
+    min_p: Faker::Number.between(1, 3),
+    max_p: Faker::Number.between(6, 14),
+    description: GameOfThrones.quote,
+    name: GameOfThrones.character 
+  )
+  post.save!
+end
+puts "finished!"
