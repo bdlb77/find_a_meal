b59ddedf7e3176f765d783eca42ff65ca2ad4ad3
Bryan Leighton <bryan.leigh77@gmail.com>
2018-06-05 13:45:24 +0300

    nested booking and routed from booking form to booking show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,7 @@ class EventsController < ApplicationController
   end
 
   def show
+    @user = User.find(current_user.id)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events, only: [:index, :show, :new, :create, :edit, :delete] 
-  resources :users do
+  resources :users 
+  resources :events, only: [:index, :show, :new, :create, :edit, :delete] do
   	resources :bookings, only: [:new, :create, :show, :index, :edit, :delete]
   end
 end
